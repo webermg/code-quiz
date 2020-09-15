8c9fc023ee9296312ac4e232ca987048ba5fc115
webermg <matthew.gerald.weber@gmail.com>
2020-09-15 09:02:36 -0700

    added javascript files and linked in html

---
+++ js/questions.js
@@ -0,0 +1,97 @@
+export let questions = [
+    [
+        "\"NaN === NaN\" returns: ",
+        [
+            "true",
+            "false"
+        ],
+        "false"
+    ],
+    [
+        "\"parseInt(\"monkey245ab7\")\" returns:",
+        [
+            "undefined",
+            "245",
+            "2457",
+            "7",
+            "NaN"
+        ],
+        "NaN"
+    ],
+    [
+        "Which of the following is a primitive type",
+        [
+            "Number",
+            "Object",
+            "Event",
+            "Array",
+        ],
+        "Number"
+    ],
+    [
+        "A const variable must be assigned a value at creation",
+        [
+            "true",
+            "false"
+        ],
+        "true"
+    ],
+    [
+        "Javascript implementation is browser-specific",
+        [
+            "true",
+            "false"
+        ],
+        "true"
+    ],
+    [
+        "Which data structure is ideal for frequent lookups",
+        [
+            "array",
+            "tree",
+            "stack",
+            "queue",
+            "map"
+        ],
+        "map"
+    ],
+    [
+        "What HTML tag is used to link Javascript?",
+        [
+            "<script>",
+            "<link>",
+            "<ref>",
+            "<div>",
+            "<external>"
+        ],
+        "<script>"
+    ],
+    [
+        "Javascript defaults to integer division",
+        [
+            "true",
+            "false"
+        ],
+        "false"
+    ],
+    [
+        "Which library contains the functions max, floor, and random",
+        [
+            "window",
+            "Math",
+            "document",
+            "numbers",
+            "Array"
+        ],
+        "Math"
+    ],
+    [
+        "Compared to Java, Javascript is: ",
+        [
+            "faster",
+            "slower",
+            "about the same"
+        ],
+        "slower"
+    ]
+]
---
+++ js/script.js
@@ -0,0 +1,1 @@
+import questions from questions.js;
