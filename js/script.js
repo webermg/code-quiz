44ee74624bcb90b74bc07ae0b40d388db6f4e1ce
webermg <matthew.gerald.weber@gmail.com>
2020-09-15 18:23:20 -0700

    added init() function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-import questions from questions.js;
+import {questions} from "./questions.js";
+
+//variables
+const timeSection = document.querySelector(".time");
+const buttons = document.querySelector(".buttons");
+
+const init = () => {
+    timeSection.textContent = "0";
+    for (const item of buttons.children) {
+        item.style.display = "none";
+    }
+}
+
+init();
