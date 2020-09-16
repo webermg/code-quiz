0ebb49584ee8368aa43f9a40159a8f18e701b80a
webermg <matthew.gerald.weber@gmail.com>
2020-09-15 18:46:26 -0700

    added handler to start button

--- js/script.js
+++ js/script.js
@@ -2,13 +2,32 @@ import {questions} from "./questions.js";
 
 //variables
 const timeSection = document.querySelector(".time");
-const buttons = document.querySelector(".buttons");
+const textArea = document.querySelector(".text");
+const startButton = document.querySelector(".start-button");
+const buttonsArea = document.querySelector(".buttons");
+const buttons = buttonsArea.children;
+let index = 0;
 
 const init = () => {
     timeSection.textContent = "0";
-    for (const item of buttons.children) {
+    for (const item of buttons) {
         item.style.display = "none";
     }
 }
+
+startButton.addEventListener("click", () => {
+    document.getElementById("heading").style.display = "none";
+    startButton.style.display = "none";
+    
+    textArea.textContent = questions[index][0];
+    for(let j = 0; j < buttons.length; j++) {
+        if(questions[index][1][j]) {
+            buttons[j].innerHTML = questions[index][1][j];
+            buttons[j].style.display = "block";
+        }
+        else buttons[j].style.display = "none";
+    }
+    
+});
 
 init();
